81e2e32cb18a1cab103e034cb1a077ff33841cab
maribelniok <mariannabelniok@outlook.com>
2023-08-22 21:16:52 -0300

    feat: add form service

---
+++ src/app/app-form.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Injectable({providedIn: 'root'})
+export class AppFormService {
+  public fb = inject(FormBuilder);
+
+  public form = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required, Validators.email],
+    phone: ['', Validators.required, Validators.pattern(/^(\+\d\s)(\d{3}\s){2}(\d{3})$/g)],
+  });
+}
