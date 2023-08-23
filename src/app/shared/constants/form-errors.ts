a866385197ad4ad15f42a28dbf294848eb8d1d21
maribelniok <mariannabelniok@outlook.com>
2023-08-23 07:30:31 -0300

    fix: mask error

--- src/app/shared/constants/form-errors.ts
+++ src/app/shared/constants/form-errors.ts
@@ -4,6 +4,7 @@ export const defaultErrors = {
   required: () => `This field is required`,
   email: () => `This email is invalid`,
   pattern: () => `This field must follow the pattern`,
+  mask: ({ requiredMask }: any) => `This field must be ${requiredMask}`,
 }
 
 export const FORM_ERRORS = new InjectionToken('FORM_ERRORS', {
